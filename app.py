1937b28d4a91e5b619e497e31889b63295aa34f1
avikgupta2323-cmyk <avikgupta2323@gmail.com>
2026-06-15 05:19:17 +0000

    s

--- app.py
+++
@@ -1,7 +0,0 @@
-"""Root Streamlit entrypoint for MiniStore.
-
-Use this file when running Streamlit from the repository root:
-    streamlit run app.py
-"""
-
-from ministore import app  # noqa: F401
